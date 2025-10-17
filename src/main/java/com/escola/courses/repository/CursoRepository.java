0293f4558031dda8a97e8aaa53ebf77bff1cff35
mxteuss <ovagabundant@gmail.com>
2025-10-17 01:24:08 -0300

    feat: add curso repository

---
+++ src/main/java/com/escola/courses/repository/CursoRepository.java
@@ -0,0 +1,10 @@
+package com.escola.courses.repository;
+
+import com.escola.courses.model.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+    List<Curso> findAll();
+}
