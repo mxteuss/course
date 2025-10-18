e205ade54f93cab48d6c1fcf7668bfcaca48abb1
mxteuss <ovagabundant@gmail.com>
2025-10-18 16:13:06 -0300

    feat: add curso service

---
+++ src/main/java/com/escola/courses/service/CursoService.java
@@ -0,0 +1,10 @@
+package com.escola.courses.service;
+
+import com.escola.courses.repository.CursoRepository;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class CursoService {
+}
