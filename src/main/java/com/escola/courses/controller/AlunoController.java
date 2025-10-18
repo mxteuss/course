0fcf3ef5b7edccd24832d006722933b0c169a173
mxteuss <ovagabundant@gmail.com>
2025-10-18 16:12:37 -0300

    refactor: colocando endpoints de curso no controller apropriado

--- src/main/java/com/escola/courses/controller/AlunoController.java
+++ src/main/java/com/escola/courses/controller/AlunoController.java
@@ -3,39 +3,29 @@ package com.escola.courses.controller;
 import com.escola.courses.model.Aluno;
 import com.escola.courses.model.Curso;
 import com.escola.courses.repository.AlunoRepository;
-import com.escola.courses.repository.CursoRepository;
 import com.escola.courses.service.CursoService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/alunos")
 public class AlunoController {
 
     @Autowired
-    private CursoRepository cursoRepository;
-
     private CursoService cursoService;
 
     @Autowired
     private AlunoRepository alunoRepository;
 
-    @GetMapping("/cursos")
-    public List<Curso> getAllCourses(){
-        return cursoRepository.findAll();
-    }
 
-    @PostMapping("/cursos")
-    public Curso postCurso(@RequestBody Curso curso){
-        return cursoRepository.save(curso);
-    }
-    @PostMapping("/aluno")
+    @PostMapping("/get")
     public Aluno postAluno(@RequestBody Aluno aluno){
 
         Curso curso = new Curso();
---
+++ src/main/java/com/escola/courses/controller/CursoController.java
@@ -0,0 +1,32 @@
+package com.escola.courses.controller;
+
+
+import com.escola.courses.model.Curso;
+import com.escola.courses.repository.CursoRepository;
+import com.escola.courses.service.CursoService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cursos")
+@AllArgsConstructor
+public class CursoController {
+
+    @Autowired
+    private CursoRepository cursoRepository;
+    @Autowired
+    private CursoService cursoService;
+
+    @GetMapping("/cursos")
+    public List<Curso> getAllCourses(){
+        return cursoRepository.findAll();
+    }
+
+    @PostMapping("/cursos")
+    public Curso postCurso(@RequestBody Curso curso){
+        return cursoRepository.save(curso);
+    }
+}
