9c8c770934c67fc4ae8a2363fdb1672fdba8e504
mxteuss <ovagabundant@gmail.com>
2025-10-19 01:02:50 -0300

    feat: endpoint get alunos

--- src/main/java/com/escola/courses/controller/AlunoController.java
+++ src/main/java/com/escola/courses/controller/AlunoController.java
@@ -1,16 +1,16 @@
 package com.escola.courses.controller;
 
 import com.escola.courses.model.Aluno;
+import com.escola.courses.model.AlunoDTO;
 import com.escola.courses.model.Curso;
 import com.escola.courses.repository.AlunoRepository;
+import com.escola.courses.repository.CursoRepository;
 import com.escola.courses.service.CursoService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,23 +20,31 @@ public class AlunoController {
 
     @Autowired
     private CursoService cursoService;
-
+    @Autowired
+    private CursoRepository cursoRepository;
     @Autowired
     private AlunoRepository alunoRepository;
 
+    @GetMapping("/get")
+    public List<Aluno> get(){
+        return alunoRepository.findAll();
+    }
 
-    @PostMapping("/get")
-    public Aluno postAluno(@RequestBody Aluno aluno){
+    @GetMapping("/get/{}")
+    public Aluno getId(@PathVariable Long id){
+        return alunoRepository.findById(id).orElseThrow(() -> new RuntimeException("Aluno não encontrado"));
+    }
+    @PostMapping("/post")
+    public Aluno postAluno(@RequestBody AlunoDTO dto){
+        Curso curso = cursoRepository.findById(dto.cursoId()).
+                orElseThrow(() -> new RuntimeException("Curso não encontrado"));
 
-        Curso curso = new Curso();
-        curso.setName("Java");
-        curso.setPrice(100.00);
-        try {
-            aluno.setCursos((List<Curso>) curso);
-        }
-        catch (ClassCastException e){
-            e.printStackTrace();
-        }
+        Aluno aluno = new Aluno();
+
+        aluno.setName(dto.name());
+        aluno.setCursos(new ArrayList<>());
+        aluno.getCursos().add(curso);
+
         return alunoRepository.save(aluno);
     }
 }
