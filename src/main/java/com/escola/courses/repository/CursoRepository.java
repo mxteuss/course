ae834d557be06474f5a1b4149fdd757f9aebdbe6
mxteuss <ovagabundant@gmail.com>
2025-10-20 22:46:31 -0300

    feat: busca com query personalizada com jpql

--- src/main/java/com/escola/courses/repository/CursoRepository.java
+++ src/main/java/com/escola/courses/repository/CursoRepository.java
@@ -2,9 +2,13 @@ package com.escola.courses.repository;
 
 import com.escola.courses.model.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CursoRepository extends JpaRepository<Curso, Long> {
+
+    @Query("SELECT p from Curso p WHERE p.price = 0")
+    List<Curso> cursosGratuitos();
     List<Curso> findAll();
 }
