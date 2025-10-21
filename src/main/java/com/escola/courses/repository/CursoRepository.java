4d478389285645d5d5174c8150e3a20a4ffeb388
mxteuss <ovagabundant@gmail.com>
2025-10-20 23:33:37 -0300

    refactor: filtrando por preço modificável

--- src/main/java/com/escola/courses/repository/CursoRepository.java
+++ src/main/java/com/escola/courses/repository/CursoRepository.java
@@ -3,12 +3,13 @@ package com.escola.courses.repository;
 import com.escola.courses.model.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CursoRepository extends JpaRepository<Curso, Long> {
 
-    @Query("SELECT p from Curso p WHERE p.price = 0")
-    List<Curso> cursosGratuitos();
+    @Query("SELECT p from Curso p WHERE p.price = :priceInput")
+    List<Curso> cursosPorPreco(@Param("priceInput") int priceInut);
     List<Curso> findAll();
 }
