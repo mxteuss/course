db013ed16561686be08171be2998a0b34561bad8
mxteuss <ovagabundant@gmail.com>
2025-10-22 21:26:26 -0300

    feat: método para pegar todos os alunos

--- src/main/java/com/escola/courses/repository/AlunoRepository.java
+++ src/main/java/com/escola/courses/repository/AlunoRepository.java
@@ -3,5 +3,8 @@ package com.escola.courses.repository;
 import com.escola.courses.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+    List<Aluno> findAll();
 }
